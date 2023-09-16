dbc18f4dced87662d6f37c998b5cc7f84da3c618
Nicolas <nicolasandrade.de.freitas@gmail.com>
2023-09-16 20:03:17 -0300

    feat: notification controller

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,25 @@
+import { Notification } from '@application/entities/notification';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+import { Injectable } from '@nestjs/common';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
+
+@Injectable()
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(notificationId: string) {
+    throw new Error('Method not implemented.');
+  }
+  async create(notification: Notification) {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notification.create({
+      data: raw,
+    });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
